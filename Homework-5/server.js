cfcd670141e9690f41bd517aaa9279b1a6ac87e7
Ebrar Erdem <ebrarerdem32@gmail.com>
2022-03-10 16:58:01 +0300

    server.js editted

--- Homework-5/server.js
+++ Homework-5/server.js
@@ -1,9 +1,10 @@
+// Call the http module for make our server
 const http = require("http");
 
 const server = http.createServer((req, res) => {
 
     const url = req.url;
-
+//Goes to which url is
     if (url === "/") {
         res.writeHead(200, {'Content-Type':'text/html'} );
         res.write("<h2>Anasayfaya Hoşgeldiniz</h2>")
@@ -31,7 +32,7 @@ const server = http.createServer((req, res) => {
 
     res.end();
 });
-
+//Port number
 const port = 5000;
 
 server.listen(port, () => {
